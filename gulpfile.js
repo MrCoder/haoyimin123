4a727737e9299b58e9475f633704e8efd8a8731b
MrCoder <eagle.xiao@gmail.com>
2015-03-01 13:53:24 +1100

    removed support files

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var deploy = require('gulp-gh-pages');
-var options = {origin: 'github'}; 
-gulp.task('deploy', function () {
-    return gulp.src(['./**/*', '!./node_modules/**'])
-        .pipe(deploy(options));
-});
