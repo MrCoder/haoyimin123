ed2c52e97ec12925e372106b2cc18142e7fd18a8
MrCoder <eagle.xiao@gmail.com>
2015-03-01 13:52:27 +1100

    restructured files

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -9,9 +9,83 @@
  */
 angular.module('haoyiminApp')
   .controller('MainCtrl', function ($scope) {
+
+    //var age = getAge($scope.birthday);
+    $scope.birthday = new Date("01/01/1980");
+
+    $scope.points = $scope.birthday;
+    console.log("$scope in controller:" + $scope);
+
+
+    $scope.getPoints = function(){
+      $scope.total="{color:red}";
+      var age = $scope.age;
+      var agePoints = 0;
+      if (age >= 25 && age <= 32){
+        agePoints = 30;
+      } else if (age >= 33 && age <= 39){
+        agePoints = 25;
+      }
+      var ssPoints = $scope.stateSponsorship ? 5 : 0;
+      var sbPoints = $scope.spouseBonus ? 5 : 0;
+      console.log("Age:" + agePoints);
+      console.log("SS:" + $scope.stateSponsorship);
+      console.log("Emp Years:" + $scope.empYearsPoints);
+      var points = parseInt(agePoints, 10) + parseInt($scope.ieltsPoints, 10) + parseInt($scope.eduPoints, 10) + parseInt(ssPoints, 10) + parseInt(sbPoints, 10) + parseInt($scope.empYearsPoints, 10);
+      if (isNaN(points)){
+        points = 0;
+      }
+      return points;
+    };
+
+
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
   });
+// bind module name, must provide '[]'
+angular.module("App", ['ui']);
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+function controller($scope){
+  //var age = getAge($scope.birthday);
+  $scope.birthday = new Date("01/01/1980");
+
+  $scope.points = $scope.birthday;
+  console.log("$scope in controller:" + $scope);
+
+
+  $scope.getPoints = function(){
+    $scope.total="{color:red}";
+    var age = getAge($scope.birthday);
+    var agePoints = 0;
+    if (age >= 25 && age <= 32){
+      agePoints = 30;
+    } else if (age >= 33 && age <= 39){
+      agePoints = 25;
+    }
+    var ssPoints = $scope.stateSponsorship ? 5 : 0;
+    var sbPoints = $scope.spouseBonus ? 5 : 0;
+    console.log("Age:" + agePoints);
+    console.log("SS:" + $scope.stateSponsorship);
+    console.log("Emp Years:" + $scope.empYearsPoints);
+    var points = parseInt(agePoints, 10) + parseInt($scope.ieltsPoints, 10) + parseInt($scope.eduPoints, 10) + parseInt(ssPoints, 10) + parseInt(sbPoints, 10) + parseInt($scope.empYearsPoints, 10);
+    if (isNaN(points)){
+      points = 0;
+    }
+    return points;
+  };
+
+}
