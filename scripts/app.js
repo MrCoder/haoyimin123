033f58f3c05bf253ea2fefdd54cb55088091fcc1
MrCoder <eagle.xiao@gmail.com>
2015-03-01 12:41:12 +1100

    move resources out of app folder

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name haoyiminApp
+ * @description
+ * # haoyiminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('haoyiminApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name haoyiminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the haoyiminApp
+ */
+angular.module('haoyiminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
